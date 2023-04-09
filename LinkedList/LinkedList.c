f4354ad849ae2f0422b9ee0022410d8fce3e7d8f
Badak <dltmdwls122789@gmail.com>
2023-04-10 00:24:31 +0900

    add new_linked_list function

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct linked_list_ {
 	int data;
 	struct linked_list_* next;
 }linked_list;
 
+linked_list* new_linked_list();
+
 int main() {
 
+}
+
+linked_list* new_linked_list() {
+	linked_list* tmp = (linked_list*)malloc(sizeof(linked_list));
+	tmp->data = 0;
+	tmp->next = NULL;
+	return tmp;
 }
